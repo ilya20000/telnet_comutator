d7c6bd1002e8b66bad701836710492339104efaf
i.sankov <i.sankov@stroylandiya.ru>
2023-02-10 17:25:21 +0500

    1

--- telnet.py
+++ telnet.py
@@ -12,34 +12,64 @@ def createParser ():
     parser.add_argument ('-m', '--mag', nargs='?')
     parser.add_argument ('-a', '--adduser', nargs='?')
     parser.add_argument ('-d', '--deluser', nargs='?')
-    parser.add_argument ('-p', '--pass', nargs='?')
+    parser.add_argument ('-p', '--passw', nargs='?')
     parser.add_argument ('-r', '--rights', nargs='?')
     return parser
 
+def connectComutator(ipComutator):
+    user = 'admin'
+    password = '123'
+    timeout = 5
 
-def whatToDo(arguments):
-    if arguments.adduser != None :
-        return 'adduser'
-    elif arguments.deluser != None :
-        return 'deluser'
+    try:
+        tn = Telnet(ipComutator, 23, timeout)
+    except Exception as e:
+        print("error: %s " % str(e))
+        if str(e) == "timed out":
+            print("connect via ssh")
+
+    try:
+        #tn.write(b"\n")
+        print("Соединяемся с "+ipComutator)
+        tn.read_until(b"UserName:",5)
+        tn.write(user.encode('ascii') + b"\n")
+        print(tn.read_very_eager())
+        tn.read_until(b"PassWord:",5)
+        tn.write(password.encode('ascii') + b"\n")
+        tn.write(b"\n")
+        print(tn.read_very_eager())
+        tn.read_until(b"admin#",5)
+        print(tn.read_very_eager())
+        return tn  
+    except Exception as e:
+        print("error: %s " % str(e))
+        if str(e) == "timed out":
+            print("connect via ssh")
+        return False
+
 
 
 def doOperation(arguments):
-    operation = whatToDo(arguments) #Какую операцию нужно выполнить? Добавление-удаление.
-    if operation == 'adduser':
-        addUser(arguments)
+    listComutators = getMagComutators(arguments.mag)
+    for ipComutator in listComutators:
+        
+        print(ipComutator+"\n")
+        tn = connectComutator(ipComutator)
+        if tn == False:
+            exit()
 
-def addUser():
-    #tn.write(b"\n")
-    tn.read_until(b"UserName:",5)
-    tn.write(user.encode('ascii') + b"\n")
-    tn.read_until(b"PassWord:",5)
-    tn.write(password.encode('ascii') + b"\n")
-    tn.write(b"\n")
-    tn.read_until(b"admin#",5)
+        if arguments.adduser != None :
+            addUser(tn, arguments)
+        elif arguments.deluser != None :
+            return 'deluser'
+            
 
+
+
+def addUser(tn, arguments):
     #tn.write(b"show account\n")
-    tn.write(b"create account operator test123 encrypt sha_1 7868dd04d3a9e04ace1c6c8c6af1c05202d5c154\n")
+    com = str("create account "+ arguments.rights +" "+ arguments.adduser +" encrypt sha_1 "+ arguments.passw +"\n")
+    tn.write( bytes(com, encoding='utf-8') )
     print(tn.read_very_eager())
     sleep(2)
     #print(tn.read_very_eager())
@@ -48,24 +78,13 @@ def addUser():
     #print(tn.read_very_eager())
     tn.close()
 
+def getMagComutators(mag):
+    print("Получаем список ip комутаторов на магазине №"+ mag)
+    return ['10.2.2.4']
 
 if __name__ == '__main__':
     parser = createParser()
     arguments = parser.parse_args(sys.argv[1:])
     doOperation(arguments)
-    print ("Привет, {}!".format (namespace.mag) )
-
-
-
-exit()
-user = 'admin'
-password = '123'
-timeout = 5
-try:
-    tn = Telnet('10.2.2.4', 23, timeout)
-except Exception as e:
-    print("error: %s " % str(e))
-    if str(e) == "timed out":
-        print("connect via ssh")
-
-
+    print ("Привет, {}!".format (arguments.mag) )
+    exit()
